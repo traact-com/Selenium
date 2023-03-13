a078139ada07a8115c849f8fbacda7d8ecf9aa89
priyanka <priyanka@192.168.1.4>
2023-03-13 23:00:08 +0530

    new test cases added for forgot password

---
+++ Automation_scripts/src/demo/ForgotPassword.java
@@ -0,0 +1,38 @@
+package demo;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+
+public class ForgotPassword {
+public static void main(String[] args) {
+		
+		
+        WebDriver driver = new ChromeDriver();
+        
+        
+        driver.manage().window().maximize();
+		  
+        driver.get("https://traact-staging.vercel.app/signin");
+        
+        //clicking on terms and conditions
+        
+         driver.findElement(By.cssSelector("#__next > div > div.MuiGrid-root.MuiGrid-item.MuiGrid-grid-sm-6.css-1pyleg0 > div > div.MuiGrid-root.MuiGrid-container.css-ruh28a > div > form > div.MuiGrid-root.MuiGrid-container.css-1d3bbye > div.MuiGrid-root.MuiGrid-item.MuiGrid-grid-xs-6.css-zapnwe > a")).click();
+	        WebElement emailtxt = driver.findElement(By.cssSelector("#__next > div > div > div > div.MuiFormControl-root.MuiFormControl-marginNormal.MuiFormControl-fullWidth.MuiTextField-root.css-wveypn > div"));
+	       
+	        Assert.assertTrue(emailtxt.isDisplayed());
+	        emailtxt.sendKeys("priyanka@traact.com");
+	        
+	       driver.findElement(By.xpath("/button[@id=':r4:']")).click();
+	      
+         //Verify Page
+         
+         String actual_url = driver.getCurrentUrl();
+			String expected_url = "https://traact-staging.vercel.app/forgot";
+			Assert.assertEquals(actual_url, expected_url,"_Error Message_");            
+	
+			
+}
+}
